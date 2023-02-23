e878efd872eed3fc4a038e98789feffc071b1312
Sherif A Shahin <125300187+SherifAShahin@users.noreply.github.com>
2023-02-23 16:39:42 +0200

    Update call_example.dart

--- call_example.dart
+++ call_example.dart
@@ -1,5 +1,6 @@
 //import textformfield_template_file_path
 
+//username
 defaultTextFormField(
                   controller: userNameController,
                   keyboardType: TextInputType.text,
@@ -10,6 +11,27 @@ defaultTextFormField(
                     if(value!.isEmpty)
                     {
                       return 'user name should not be empty!';
+                    }
+                    return null;
+                  },
+                )
+//password
+defaultTextFormField(
+                  controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  label: 'Password',
+                  isPassword: isPassword,
+                  suffixIcon: isPassword ? Icons.visibility_off : Icons.visibility,
+                  suffixPressed: (){
+                    setState(() {
+                      isPassword = !isPassword;
+                    });
+                  },
+                  validate: (value)
+                  {
+                    if(value!.isEmpty)
+                    {
+                      return 'password should not be empty!';
                     }
                     return null;
                   },
